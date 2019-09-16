765ba9688055d7e7f661b7ae26edb517490d7c6a
Kenneth Hau <47057278+kitchun0402@users.noreply.github.com>
2019-09-16 23:24:45 +0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+import setuptools
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+setuptools.setup(
+     name='igenemy',  
+     version='0.0.1',
+     scripts=['igenemy'] ,
+     author="Kenneth Hau",
+     author_email="kitchun0402@gmail.com",
+     description="WebScraping_Instagram",
+     long_description=long_description,
+   long_description_content_type="text/markdown",
+     url="https://github.com/kitchun0402/WebScraping_Instagram_igenemy",
+     packages=setuptools.find_packages(),
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+     ],
+ )
