946298de7582d17b8117808f465edea266c8155d
Kenneth Hau <hkc930402@gmail.com>
2019-09-17 12:05:31 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 setuptools.setup(
      name='igenemy',  
-     version='0.1',
+     version='0.2',
      scripts=['igenemy'] ,
      author="Kenneth Hau",
      author_email="kitchun0402@gmail.com",
