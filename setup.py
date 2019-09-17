011f92432494642b54fcbe990e976d058cb7cd83
Kenneth Hau <hkc930402@gmail.com>
2019-09-17 14:40:52 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,20 @@
 from setuptools import setup
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
 setup(
-     name='igenemy',  
-     version='0.1',
-     author="Kenneth Hau",
-     author_email="kitchun0402@gmail.com",
-     description="WebScraping_Instagram",
-     url="https://github.com/kitchun0402/WebScraping_Instagram_igenemy",
-     license = 'MIT',
-     zip_safe = False,
-     packages=['igenemy'],
-     dependency_links = ['https://pypi.org/project/selenium/', 'https://pypi.org/project/bs4/',
+    name='igenemy',  
+    version='0.1',
+    author="Kenneth Hau",
+    author_email="kitchun0402@gmail.com",
+    description="WebScraping_Instagram",
+    url="https://github.com/kitchun0402/WebScraping_Instagram_igenemy",
+    license = 'MIT',
+    zip_safe = False,
+    packages=['igenemy'],
+    dependency_links = ['https://pypi.org/project/selenium/', 'https://pypi.org/project/bs4/',
                         'https://pypi.org/project/ipython/'],
-     python_requires = '>= 3'
+    python_requires = '>= 3',
+    long_description=long_description,
+    long_description_content_type="text/markdown"
  )
