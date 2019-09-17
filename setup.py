4490383c3292908759fa72bf34b52bc0aa3d53d8
Kenneth Hau <hkc930402@gmail.com>
2019-09-17 14:30:01 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,5 +10,7 @@ setup(
      license = 'MIT',
      zip_safe = False,
      packages=['igenemy'],
+     dependency_links = ['https://pypi.org/project/selenium/', 'https://pypi.org/project/bs4/',
+                        'https://pypi.org/project/ipython/'],
      python_requires = '>= 3'
  )
